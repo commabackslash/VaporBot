ab6972f35e32be9266041c939aada49b06498d25
commabackslash <93020842+commabackslash@users.noreply.github.com>
2022-03-06 12:53:15 -0500

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # VaporBot
+
+In development bot for discord.gg/mwah
+
+Current features:
+
+ - Blacklisted words that will mute user for set time period
+
+
+
+
+Future features:
+
+ - Vanity watcher
+ - New account banning
+ - Content Scanning
---
+++ vaporbot.py
@@ -0,0 +1,42 @@
+#written by ,\#0001
+
+import discord
+import discord.utils
+import discord.role
+import time
+import asyncio
+global wordlist
+
+## CONFIG
+token = "" #Bot token
+server_id = "" #Server(Guild) id
+wordlist = ("word1", "word2", "word3") #Words to blacklist (CHANGE THIS)
+mute_time = 15 #Minutes
+
+seconds = mute_time * 60
+word = ""
+client = discord.Client()
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == server_id:
+            break
+    print('[*]' + ' Connected to Discord!')        
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="myko"))
+
+@client.event
+async def on_message(message):
+
+    for word in wordlist:
+        if word in message.content:
+            await message.delete()
+            await message.channel.send("> **No slurs please " + str(message.author) + ", muted for " + str(mute_time) + " minutes.**")
+            member = message.author
+            role = discord.utils.get(message.guild.roles, name = "Muted")
+            await member.add_roles(role)
+            print("[*] " + str(message.author) + " has been muted for saying " + str(message.content))
+            if time:
+                await asyncio.sleep(seconds)
+                await member.remove_roles(role)
+
+client.run(token)
